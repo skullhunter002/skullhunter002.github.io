0cdd5eb266601aedf35ca6553c319e60d166dc5d
vishal waghchaure <157978211+skullhunter002@users.noreply.github.com>
2024-02-05 23:27:14 +0530

    0

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-        $con = mysqli_connect("localhost","root","","digital_library");
-      
-        if (mysqli_connect_errno()) {
-                echo "Failed to connect to MySql " . mysqli_connect_error();
-        }
-?>
